c6afe71ad83d5318398628fff425b049c7b692d7
CrazedMiner <daswaggamer@gmail.com>
2015-04-13 01:51:44 +1000

    Update Main.php

--- PvP-Stats/src/CrazedMiner/Main.php
+++ PvP-Stats/src/CrazedMiner/Main.php
@@ -23,6 +23,8 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 use pocketmine\Player;
 use pocketmine\event\player\PlayerDeathEvent
+use pocketmine\utils\Config;
+
 
 class Main extends PluginBase implements Listener {
   
@@ -36,13 +38,34 @@ class Main extends PluginBase implements Listener {
     }
     
     public function onDisable(){
-      $this->getLogger()->info(TextFormat::RED . "PvP Stats v1.0.0 by CrazedMiner is now Disabled!");
+        $this->getLogger()->info(TextFormat::RED . "PvP Stats v1.0.0 by CrazedMiner is now Disabled!");
       }
     
     public function onPlayerDeath(PlayerDeathEvent $event){
-      $victim = $event->getEntity();
-      if($victim instanceof Player){
-          
+      $v = $event->getEntity();
+          if($v instanceof Player){
+          $vd = new Config($this->getDataFolder() . "data/" . strtolower($v->getPlayer()->getName()) . ".txt", Config::ENUM);
+            if($vd->exists("Kills") && $vd->exists("Deaths"){
+                $vd->set("Deaths", $vd->get("Deaths") +1);
+                $vd->save();
+         }else{
+          $vd->setAll(array("Kills" =>0, "Deaths" =>1));
+          $vd->save();
+                 }
+        $c = event->getEntity()->getLastDamageCause()->getCause();
+        if($c == 1){
+            $k = $event->getEntity()->getLastDamageCause()->getDamager();
+            if($k instanceof Player){
+                $kd = new Config($this->getDataFolder() . "data/" . strtolower($k->getPlayer()->getName()) . ".txt", Config::ENUM);
+              if($kd->exists("Kills") && $kd->exists("Deaths")){
+                    $kd->set("Kills", $kd->get("Kills") +1);
+               $kd->save();
+              }else{
+               $kd->setAll(array("Kills" => 1, "Deaths" => 0));
+               $kd->save();
+                      }
+            }
+        }
         }
       }
     
