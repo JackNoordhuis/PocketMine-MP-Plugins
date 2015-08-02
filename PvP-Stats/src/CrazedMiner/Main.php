f97f02db37641ba4588b1221fb9893989f92c0a3
CrazedMiner <daswaggamer@gmail.com>
2015-08-02 23:20:03 +1000

    Update Main.php

--- PvP-Stats/src/CrazedMiner/Main.php
+++ PvP-Stats/src/CrazedMiner/Main.php
@@ -16,57 +16,82 @@
  * GNU General Public License for more details.
 */
 
-
-namespace \CrazedMiner;
+namespace CrazedMiner;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
-use pocketmine\Player;
-use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\utils\Config;
+use pocketmine\IPlayer;
+
+use CrazedMiner\StatsCommand;
+use CrazedMiner\Providers\ProviderInterface;
+use CrazedMiner\Providers\YAMLProvider;
+use CrazedMiner\Providers\MYSQLProvider;
+
+class Main extends PluginBase {
+    
+    protected $provider;
 
 
-class Main extends PluginBase implements Listener {
-  
-  public function onLoad(){
-      $this->getLogger()->info(TextFormat::YELLOW . "Loading PvP Stats v1.0.0 by CrazedMiner....");
-  }
-  
-    public function onEnable(){
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getLogger()->info(TextFormat::GREEN . "PvP Stats v1.0.0 by CrazedMiner is now Enabled!");
+    public function onEnable() {
+        $this->getCommand("stats")->setExecutor(new StatsCommand($this));
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+        $this->saveResource("Settings.yml");
+        $this->setProvider();
+        $this->getLogger()->info(TextFormat::GREEN . "PvP-Stats v1.0 by CrazedMiner is now Enabled!");
     }
     
-    public function onDisable(){
+    public function onDisable() {
         $this->getLogger()->info(TextFormat::RED . "PvP Stats v1.0.0 by CrazedMiner is now Disabled!");
     }
     
-    public function onPlayerDeath(PlayerDeathEvent $event){
-        $v = $event->getEntity();
-            if($v instanceof Player){
-                $vd = new Config($this->getDataFolder() . "data/" . strtolower($v->getPlayer()->getName()) . ".txt", Config::ENUM);
-                    if($vd->exists("Kills") && $vd->exists("Deaths"){
-                        $vd->set("Deaths", $vd->get("Deaths") +1);
-                        $vd->save();
-                    }else{
-                        $vd->setAll(array("Kills" =>0, "Deaths" =>1));
-                        $vd->save();
-                    }
-                $c = event->getEntity()->getLastDamageCause()->getCause();
-                if($c == 1){
-                    $k = $event->getEntity()->getLastDamageCause()->getDamager();
-                        if($k instanceof Player){
-                            $kd = new Config($this->getDataFolder() . "data/" . strtolower($k->getPlayer()->getName()) . ".txt", Config::ENUM);
-                                if($kd->exists("Kills") && $kd->exists("Deaths")){
-                                    $kd->set("Kills", $kd->get("Kills") +1);
-                                    $kd->save();
-                        }else{
-                            $kd->setAll(array("Kills" => 1, "Deaths" => 0));
-                            $kd->save();
-                        }
-               }
-           }
+    public function setProvider() {
+        $provider = (new Config($this->getDataFolder() . "Settings.yml"))->getAll()["data-provider"];
+        unset($this->provider);
+        
+        switch(strtolower($provider)) {
+            case "yaml":
+                $provider = new YAMLProvider($this);
+                break;
+            case "mysql":
+                $provider = new MYSQLProvider($this);
+                break;
         }
-     }
+        
+        if(!isset($this->provider) or !($this->provider instanceof ProviderInterface)) {
+            $this->provider = $provider;
+        }else {
+            $this->getLogger()->critical("Data Provider error!");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+        }
+    }
+    
+    public function getProvider() {
+        return $this->provider;
+    }
+    
+    public function getPlayer(IPlayer $player) {
+        return $this->provider->getPlayer($player);
+    }
+    
+    public function getData($player) {
+        return $this->provider->getData($player);
+    }
+    
+    public function playerExists(IPlayer $player) {
+        return $this->provider->playerExists($player);
+    }
+    
+    public function addPlayer(IPlayer $player) {
+        return $this->provider->addPlayer($player);
+    }
+    
+    public function removePlayer(IPlayer $player) {
+        return $this->provider->removePlayer($player);
+    }
+    
+    public function updatePlayer(IPlayer $player, $type) {
+        return $this->provider->updatePlayer($player, $type);
+    }
     
 }
