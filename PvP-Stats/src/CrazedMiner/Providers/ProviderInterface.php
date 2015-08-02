8156e207309dbfe475973c299725e931bc1d01d6
CrazedMiner <daswaggamer@gmail.com>
2015-08-02 23:25:36 +1000

    Update ProviderInterface.php

--- PvP-Stats/src/CrazedMiner/Providers/ProviderInterface.php
+++ PvP-Stats/src/CrazedMiner/Providers/ProviderInterface.php
@@ -1,5 +1,21 @@
 <?php
 
+/*
+ * PvP-Stats plugin for PocketMine-MP
+ * Copyright (C) 2014 Jack Noordhuis (CrazedMiner) 
+ * <https://github.com/CrazedMiner/PocketMine-MP-Plugins/tree/master/PvP-Stats>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+*/
+
 namespace CrazedMiner\Providers;
 
 use pocketmine\IPlayer;
