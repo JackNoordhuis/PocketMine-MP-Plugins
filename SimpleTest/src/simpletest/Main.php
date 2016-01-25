ca124d8fa9f1c52fcbaaf2a5424fce1380ab107e
CrazedMiner <daswaggamer@gmail.com>
2016-01-26 00:05:00 +1100

    Adds an extremely basic plugin for testing things
    This plugin will help developers find an average speed for a player and
    an average reach. This is how Spartans anti hack values were found.

---
+++ SimpleTest/src/simpletest/Main.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace simpletest;
+
+use pocketmine\plugin\PluginBase;
+
+use simpletest\EventListener;
+
+class Main extends PluginBase {
+        
+        public function onEnable() {
+                $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+        }
+        
+}
