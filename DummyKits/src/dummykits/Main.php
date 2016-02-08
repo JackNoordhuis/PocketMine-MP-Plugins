699f1ec5eb992b002b308ab1d1722bd30c861066
CrazedMiner <daswaggamer@gmail.com>
2016-02-08 18:01:42 +1100

    Start rewrite on DummyKits

---
+++ DummyKits/src/dummykits/Main.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace dummykits;
+
+use pocketmine\PluginBase;
+
+class Main extends PluginBase {
+        
+        public function onEnable() {
+                
+        }
+        
+}
