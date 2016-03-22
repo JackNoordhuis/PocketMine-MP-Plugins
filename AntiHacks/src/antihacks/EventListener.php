fc2779f33a0b7ec5f5aa1f1bde0dd9f76d074823
CrazedMiner <daswaggamer@gmail.com>
2016-03-22 21:19:25 +1100

    AntiHacks v1.0.0 - release
    Adds a new and extremely basic antihack plugin, this well help protect
    your server from fly modders and jump modders.

---
+++ AntiHacks/src/antihacks/EventListener.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * EventListener class
+ * 
+ * Created on Mar 22, 2016 at 8:10:52 PM
+ *
+ * @author Jack
+ */
+
+namespace antihacks;
+
+use pocketmine\event\Listener;
+use pocketmine\entity\Effect;
+
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerKickEvent;
+
+use antihacks\Main;
+
+class EventListener implements Listener {
+        
+        /** @var $plugin Main */
+        private $plugin;
+        
+        /**
+         * 
+         * Construct a new EventListener class
+         * 
+         * @param Main $plugin
+         */
+        public function __construct(Main $plugin) {
+                $this->plugin = $plugin;
+                $plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
+        }
+        
+        /**
+         * 
+         * Get owning plugin instance
+         * 
+         * @return Main
+         */
+        public function getPlugin() {
+                return $this->plugin;
+        }
+        
+        /**
+         * 
+         * @param PlayerMoveEvent $event
+         * 
+         * @return null
+         * 
+         * @priority HIGHEST
+         */
+        public function onMove(PlayerMoveEvent $event) {
+                $player = $event->getPlayer();
+                if($event->isCancelled() or $player->hasPermission("antifly.exempt")or $player->isCreative() or $player->isSpectator() or $player->getAllowFlight() or $player->hasEffect(Effect::JUMP)) {
+                        return;
+                }  else {
+                        if(($player->getInAirTicks() * 20) >= $this->plugin->getSettings()["in-air-threshold"]) {
+                                $player->kick(Main::applyColor($this->plugin->getSettings()["messages"]["flying-kick"]), false);
+                        } elseif(abs($event->getFrom()->y - $event->getTo()->y) >= $this->plugin->getSettings()["jump-blocks"]) {
+                                $this->plugin->addTag($player);
+                        }
+                        if($this->plugin->checkTag($player)) {
+                                $player->kick(Main::applyColor($this->plugin->getSettings()["messages"]["jumping-kick"]), false);
+                        }
+                }
+                return;
+        }
+        
+        /**
+         * @param PlayerQuitEvent $event
+         * 
+         * @return null;
+         */
+        public function onQuit(PlayerQuitEvent $event) {
+                $this->plugin->removeTag($event->getPlayer());
+                return;
+        }
+        
+        /**
+         * @param PlayerKickEvent $event
+         * 
+         * @return null
+         */
+        public function onKick(PlayerKickEvent $event) {
+                $this->plugin->removeTag($event->getPlayer());
+                return;
+        }
+}
