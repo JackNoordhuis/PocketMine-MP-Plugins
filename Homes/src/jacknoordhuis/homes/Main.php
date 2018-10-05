0dc882f7385d8249554686c077991014738b2593
Jack Noordhuis <daswaggamer@gmail.com>
2018-10-05 17:13:40 +1000

    Update Homes plugin to API 3.0.0

--- Homes/src/jacknoordhuis/homes/Main.php
+++ Homes/src/jacknoordhuis/homes/Main.php
@@ -1,11 +1,17 @@
 <?php
 
 /**
- * Main.php class
+ * Main.php – PocketMine-MP-Plugins
  *
- * Created on 7/05/2016 at 11:20 PM
+ * Copyright (C) 2018 Jack Noordhuis
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * @author Jack
+ *
  */
 
 namespace jacknoordhuis\homes;
@@ -32,11 +38,11 @@ class Main extends PluginBase implements Listener {
 	 * Handles the enabling for homes
 	 */
 	public function onEnable() {
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		if(!is_dir($this->getDataFolder()) . self::DATA_FOLDER) {
 			@mkdir($this->getDataFolder() . self::DATA_FOLDER);
 		}
-		$this->getLogger()->info(TextFormat::AQUA . $this->getDescription()->getName() . TextFormat::GOLD . " v" . $this->getDescription()->getVersion() . TextFormat::GREEN . " by " . TextFormat::GOLD . $this->getDescription()->getAuthors()[0] . TextFormat::GREEN . " has been enabled!" . TextFormat::RESET);
+
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 
 	/**
@@ -49,29 +55,27 @@ class Main extends PluginBase implements Listener {
 	 *
 	 * @return bool
 	 */
-	public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
 		if($sender instanceof Player) {
 			switch(strtolower($command->getName())) {
 				case "home":
 					if($this->hasHome($sender->getName())) {
 						$sender->teleport($this->getHome($sender->getName()));
 						$sender->sendMessage(TextFormat::GREEN . "You have been teleported!");
-						return true;
 					} else {
 						$sender->sendMessage(TextFormat::RED . "You have no home set!");
-						return true;
 					}
-					break;
+					return true;
 				case "sethome":
 					$this->saveHome($sender->getName(), clone $sender->getPosition());
 					$sender->sendMessage(TextFormat::GREEN . "Home saved!");
 					return true;
-					break;
 			}
 		} else {
 			$sender->sendMessage(TextFormat::RED . "Please use this command in-game!");
 			return true;
 		}
+
 		return false;
 	}
 
@@ -82,7 +86,7 @@ class Main extends PluginBase implements Listener {
 	 *
 	 * @return Vector3
 	 */
-	public function getHome($name) {
+	public function getHome($name) : Vector3 {
 		$data = json_decode(file_get_contents($this->getDataFolder() . self::DATA_FOLDER . strtolower($name) . ".json"));
 		return new Vector3($data->x, $data->y, $data->z);
 	}
@@ -91,9 +95,10 @@ class Main extends PluginBase implements Listener {
 	 * Save a players home
 	 *
 	 * @param $name
+	 *
 	 * @param Vector3 $pos
 	 */
-	public function saveHome($name, Vector3 $pos) {
+	public function saveHome($name, Vector3 $pos) : void {
 		$file = fopen($this->getDataFolder() . self::DATA_FOLDER . strtolower($name) . ".json", "w");
 		fwrite($file, json_encode([
 			"x" => $pos->x,
@@ -110,7 +115,7 @@ class Main extends PluginBase implements Listener {
 	 *
 	 * @return bool
 	 */
-	public function hasHome($name) {
+	public function hasHome($name) : bool {
 		return is_file($this->getDataFolder() . self::DATA_FOLDER . strtolower($name) . ".json");
 	}
 
